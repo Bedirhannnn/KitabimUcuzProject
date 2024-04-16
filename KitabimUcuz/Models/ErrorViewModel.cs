9463069974cbb994428f4ed1c69dd10a82023b3e
Bedirhannnn <bedirhanyiber@gmail.com>
2024-04-16 23:21:08 +0300

    Add project files.

---
+++ KitabimUcuz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KitabimUcuz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
